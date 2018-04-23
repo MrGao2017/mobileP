618fa1ec277347b309ecf9b9395895241025ca79
MrGao2017 <1300023376@qq.com>
2018-04-23 19:56:24 +0800

    0423

---
+++ vue/2018.4.33/vue-demo/src/components/News.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="news">
+        <h3>我是新闻页面</h3>
+        <ul>
+          <li v-for="(val,index) of news" :key="index">{{val}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                news:['111','222','333','444']
+            }
+        }
+    }
+</script>
+
